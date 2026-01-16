97cb8dfb23977d650cfbc0334f6b6b64077ff4bb
Bhanuteja005 <pashikantibhanuteja@gmail.com>
2026-01-16 15:15:19 +0530

    waitlist page

--- src/components/marketing/navbar.tsx
+++ src/components/marketing/navbar.tsx
@@ -8,19 +8,19 @@ const Navbar = () => {
             <Wrapper className="h-full">
                 <div className="relative flex items-center justify-between h-full">
                     <div className="flex items-center w-1/3">
-                        {/* left spacer */}
+                        {/* spacer (mobile centered logo will be used) */}
                     </div>
 
-                    <div className="absolute left-1/2 top-1/2 transform -translate-x-[60%] -translate-y-1/2">
+                    <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center">
                         <Link href="/" className="flex items-center">
                             <Image 
-                                    src="/images/logo1.png" 
-                                    alt="Socialfly Logo" 
-                                    width={128} 
-                                    height={128} 
-                                    className="w-32 h-32 object-cover rounded-full"
-                                />
-                            <span className="text-lg font-semibold text-gray-900 leading-none ml-1">
+                                src="/images/logo1.png" 
+                                alt="Socialfly Logo" 
+                                width={96} 
+                                height={96} 
+                                className="w-24 h-24 object-cover rounded-full block"
+                            />
+                            <span className="text-lg font-semibold text-gray-900 leading-none -ml-6">
                                 Socialflyai
                             </span>
                         </Link>
